7af4046fb7518fbeab3c113f1dbadd8862491100
Jakub Libosvar <libosvar@redhat.com>
2020-02-12 17:42:48 +0100

    Remove emoticons

--- compute/dhcp_downtime.sh
+++ compute/dhcp_downtime.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#echo "Stopping triple💩_ovn_controller"
+#echo "Stopping tripleo_ovn_controller"
 systemctl stop tripleo_ovn_controller
 #echo "Sleeping 5 seconds"
 sleep 5
 #echo "Starting the awesome script in the background"
 python test_dhcp_request.py -n reference -i reference &
 script_pid=$!
-#echo "Starting triple💩_ovn_controller"
+#echo "Starting tripleo_ovn_controller"
 systemctl start tripleo_ovn_controller
 #echo "Waiting for the awesome script to finish"
 wait $script_pid
